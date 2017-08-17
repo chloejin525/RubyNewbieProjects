f42b9241dbbc726c411013cf0d8e5ee267d6e2e2
Ling Jin <chloejin525@gmail.com>
2017-08-17 11:57:12 -0400

    write a function to check different patterns

--- write_a_function.rb
+++ write_a_function.rb
@@ -1,8 +1,38 @@
 def triple_double(num1,num2)
-  return 1 if two(num2.to_s, three(num1.to_s))
+  return 0 if check_repeat(num1.to_s, 3, []) == []
+  res = check_repeat(num2.to_s, 2, check_repeat(num1.to_s, 3, []))
+  return 1 if res && res!=[]
   return 0
 end
 
+# write a function to check different patterns
+def check_repeat(str, time, pattern)
+  count = 0
+  if pattern == [] 
+    last = '0'
+    str.each_char do |c| 
+      c == last ? (count+=1) : (count=0)
+      last = c 
+      if count==time-1
+        pattern << c 
+        count = 0
+      end 
+    end
+    return pattern
+  else 
+    pattern.each do |p|
+      str.each_char do |c|
+        c == p ? (count+=1) : (count=0)
+        return true if count == time  
+      end
+    end
+  end
+  return false 
+end 
+
+
+
+=begin
 def three(str)
   count = 1
   last = '0'
@@ -28,6 +58,7 @@ def two(str, pattern)
   end
   return false 
 end
+=end 
 
 puts triple_double(2158555466834873666417,18661)
 puts triple_double(231118955543, 455590)
